2181b085d7916fc3e1a73226b1d361f4d375f034
kurianw <kurianw@192.168.1.10>
2017-05-02 11:48:41 -0700

    Subclassing Player as a child of Collidable, implementing the Resolve method.

--- square-shoots/src/Player.java
+++ square-shoots/src/Player.java
@@ -1,15 +1,19 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
-public class Player {
-public int xposition;
-public double yposition;
+public class Player extends Collidable {
 public int xspeed;
 public double yspeed;
 public String color;
 public String eyes;
 public static final double g = 0.005;
-public Player(){};
+
+public Player(String color, String eyes){
+	this.color = color;
+	this.eyes = eyes;
+};
+
 public void jump(){
 	yspeed = 1;
 };
@@ -59,6 +63,47 @@ void render(Graphics g) {
 	g.setColor(Color.decode(GetEyeColor()));
 	g.fillRect(175+ (int)getXposition(), 472+ (int)getYposition(), 5, 12);
 }
+public void setPosition(int xPosition, int yPosition) {
+	this.xposition = xPosition;
+	this.yposition = yPosition;
+	
+}
+
+public void resolve(ArrayList<Collidable> others) {
+	int new_xposition = xposition;
+	double new_yposition = yposition;
+	for (Collidable other: others) {
+		new_xposition = resolveHorizontalAxis(new_xposition, other);
+		new_yposition = resolveVerticalAxis(new_yposition, other);
+	}
+}
+
+private int resolveHorizontalAxis(int new_xposition, Collidable other) {
+	if (xspeed < 0) { //left
+		if (new_xposition < other.xposition + other.width) {
+			new_xposition = other.xposition + other.width + 1;
+		}
+	} else { //right
+		if (new_xposition + width > other.xposition) {
+			new_xposition = other.xposition - width - 1;
+		}
+	}
+	return new_xposition;
+};	
+
+
+private double resolveVerticalAxis(double new_yposition, Collidable other) {
+	if (yspeed < 0) { //up
+		if (new_yposition > other.yposition - other.height) {
+			new_yposition = other.yposition - other.height - 1;
+		}
+	} else { //down
+		if (new_yposition - height < other.yposition) {
+			new_yposition = other.yposition + height + 1;
+		}
+	}
+	return new_yposition;
+};
 
 }
 
